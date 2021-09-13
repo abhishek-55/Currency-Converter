35462305f1440df1305800be8913f97adea47c47
Abhishek kumar <abhishekbt2001@gmail.com>
2021-09-13 20:44:21 +0530

    Add country dropdown

--- src/components/Main_panel.js
+++ src/components/Main_panel.js
@@ -1,28 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import Options from "./options"
 
-export default function main_panel() {
+export default function Main_panel() {
+
+    const [fromValue , setFromValue] = useState(0.0000000000);
+    const [toValue , setToValue] = useState(0.0000000000);
+
+
+    function handleFromChange(event){
+        let value = event.target.value;
+        value = fromValue / toValue ;
+        setFromValue(value);
+    }
+    function handleToChange(event){
+        let value = event.target.value;
+        value = toValue / fromValue;
+        setFromValue(value);
+    }
+
+
+
+    async function fetchData(){
+
+        //let data = await fetch("http://api.exchangeratesapi.io/v1/latest?access_key=e3154eff7e406f71ab0bdfc3348ae7ae");
+        //let rates = await data.json();
+        //console.log(rates.rates);
+    }
+    fetchData();
     return (
         <div className="col-sm ">
-                <div class="input-group mb-2 mr-sm-2 center">
-                    <div class="input-group-prepend">
-                    <div class="input-group-text">
+                <div className="input-group mb-2 mr-sm-2 center">
+                    <div className="input-group-prepend">
+                    <div className="input-group-text">
                     <select className="form-control form-control-md">
-                    <option>EUR</option>
+                    <Options/>
                     </select>
                     </div>
                     </div>
-                    <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="0.0000000000"/>
+                    <input type="text" className="form-control" id="inlineFormInputGroupUsername2" onChange={handleFromChange} value={fromValue}/>
                 </div>
 
-                <div class="input-group mb-2 mr-sm-2 center">
-                    <div class="input-group-prepend">
-                    <div class="input-group-text">
+                <div className="input-group mb-2 mr-sm-2 center">
+                    <div className="input-group-prepend">
+                    <div className="input-group-text">
                     <select className="form-control form-control-md">
-                    <option>INR</option>
+                    <Options/>
                     </select>
                     </div>
                     </div>
-                    <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="0.0000000000"/>
+                    <input type="text" className="form-control" id="inlineFormInputGroupUsername2" onChange={handleToChange} value={toValue}/>
                 </div>
         </div>
     )
