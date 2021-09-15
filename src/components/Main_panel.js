38abe0e9f8129704ae4a4301bc4ea80ab7cea5c6
Abhishek kumar <abhishekbt2001@gmail.com>
2021-09-15 10:48:16 +0530

    Project Complete

--- src/components/Main_panel.js
+++ src/components/Main_panel.js
@@ -1,55 +1,87 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Options from "./options"
 
+
 export default function Main_panel() {
+    let rates;
+    useEffect(() => {
+        fetchData();
+    }, )
+    const [fromValue , setFromValue] = useState(0);
+    const [toValue , setToValue] = useState(0);
+    const [from_country, setFrom_country] = useState("AED");
+    const [to_country, setTo_country] = useState("AED");
+    const [fromCountryCurrencyValue, setFromCountryCurrencyValue] = useState(1.0);
+    const [toCountryCurrencyValue, setToCountryCurrencyValue] = useState(1.00);
 
-    const [fromValue , setFromValue] = useState(0.0000000000);
-    const [toValue , setToValue] = useState(0.0000000000);
-
+    function getSelectedCountry(){
+    const value1 = document.getElementById("fromValue").value;
+    const value2 = document.getElementById("toValue").value;
+    setFrom_country(value1);
+    setTo_country(value2);
+    if(typeof(rates) !== "undefined"){
+        setFromCountryCurrencyValue(parseFloat(rates[value1]));
+        setToCountryCurrencyValue(parseFloat(rates[value2]));
+        setFromValue(0);
+        setToValue(0);
+    }else{
+        setFromValue(0);
+        setToValue(0);
+    }
+    }
+    
 
     function handleFromChange(event){
-        let value = event.target.value;
-        value = fromValue / toValue ;
-        setFromValue(value);
+        const value = parseFloat(event.target.value);
+        if(!isNaN(value)){      
+        const convertedValue = (toCountryCurrencyValue / fromCountryCurrencyValue) * value;
+        setToValue(convertedValue);
+        }else{
+        setToValue("");
+        }
+        setFromValue(value);    
     }
     function handleToChange(event){
-        let value = event.target.value;
-        value = toValue / fromValue;
-        setFromValue(value);
+        const value = parseFloat(event.target.value);
+        if(!isNaN(value)){ 
+        const convertedValue = (fromCountryCurrencyValue / toCountryCurrencyValue) * value;
+        setFromValue(convertedValue);
+        }else{
+            setFromValue("");
+            }
+        setToValue(value);
     }
-
-
 
     async function fetchData(){
-
-        //let data = await fetch("http://api.exchangeratesapi.io/v1/latest?access_key=e3154eff7e406f71ab0bdfc3348ae7ae");
-        //let rates = await data.json();
-        //console.log(rates.rates);
+        let data = await fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=${process.env.REACT_APP_ACCESS_KEY}`);
+        let parsedData = await data.json();
+        rates = parsedData.rates;  
     }
-    fetchData();
+
     return (
         <div className="col-sm ">
                 <div className="input-group mb-2 mr-sm-2 center">
                     <div className="input-group-prepend">
                     <div className="input-group-text">
-                    <select className="form-control form-control-md">
+                    <select id="fromValue"  className="form-control form-control-md" onChange={getSelectedCountry}>
                     <Options/>
                     </select>
                     </div>
                     </div>
-                    <input type="text" className="form-control" id="inlineFormInputGroupUsername2" onChange={handleFromChange} value={fromValue}/>
+                    <input type="number" className="form-control" id="inlineFormInputGroupUsername2" onChange={handleFromChange} value={fromValue.toString()}/>
                 </div>
-
                 <div className="input-group mb-2 mr-sm-2 center">
                     <div className="input-group-prepend">
                     <div className="input-group-text">
-                    <select className="form-control form-control-md">
+                    <select id="toValue"  className="form-control form-control-md" onChange={getSelectedCountry}>
                     <Options/>
                     </select>
                     </div>
                     </div>
-                    <input type="text" className="form-control" id="inlineFormInputGroupUsername2" onChange={handleToChange} value={toValue}/>
+                    <input type="number" className="form-control" id="inlineFormInputGroupUsername2" onChange={handleToChange} value={toValue.toString()}/>
                 </div>
+                 
+                <h4>Converting From {from_country} to {to_country}</h4>
         </div>
     )
 }
